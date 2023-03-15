143d357c40f4d8235cfb7cd5d1f1673a76e09ad3
jungminji <wjdalswl0215@gmail.com>
2023-03-16 02:19:21 +0900

    [#10] 구매 API

--- src/main/java/com/example/timedeal/api/OrderController.java
+++ src/main/java/com/example/timedeal/api/OrderController.java
@@ -1,11 +1,13 @@
 package com.example.timedeal.api;
 
 import com.example.timedeal.dto.order.request.CreateOrderRequest;
-import com.example.timedeal.dto.order.response.CreateOrderResponse;
 import com.example.timedeal.service.order.OrderService;
 import com.example.timedeal.utils.ResponseResult;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
@@ -13,15 +15,10 @@ public class OrderController {
     private final OrderService orderService;
 
     @PostMapping("/order")
-    public CreateOrderResponse createOrder(
+    public ResponseResult<?> createOrder(
             @RequestBody CreateOrderRequest request,
             @RequestHeader("Authentication") String requestHeader
     ){
-        return orderService.order(request, requestHeader);
-    }
-
-    @GetMapping("/order/product/{productId}")
-    public ResponseResult listProduct(){
-        return orderService.listProduct();
+        return new ResponseResult<>(orderService.order(request, requestHeader));
     }
 }
