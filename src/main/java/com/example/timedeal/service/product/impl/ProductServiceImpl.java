08d9c5542e0eee9286de22c0c8b7b5f766fb46fa
jungminji <wjdalswl0215@gmail.com>
2023-03-25 13:22:01 +0900

    [#20] 피드백 반영

--- src/main/java/com/example/timedeal/service/product/impl/ProductServiceImpl.java
+++ src/main/java/com/example/timedeal/service/product/impl/ProductServiceImpl.java
@@ -1,6 +1,6 @@
 package com.example.timedeal.service.product.impl;
 
-import com.example.timedeal.common.DiscountProductService;
+import com.example.timedeal.utils.DiscountProduct;
 import com.example.timedeal.domain.product.Product;
 import com.example.timedeal.domain.product.ProductRepository;
 import com.example.timedeal.domain.product.sale.ProductSale;
@@ -20,14 +20,13 @@ import java.util.stream.Collectors;
 @Service
 public class ProductServiceImpl implements ProductService {
     private final ProductRepository productRepository;
-    private final DiscountProductService discountProductService;
 
     @Override
     @Transactional
     public CreateProductResponse createProduct(CreateProductRequest request) {
         Product product = request.toEntity(request);
 
-        int discountPrice = discountProductService.calculateProductSale(request.getPrice(), request.getProductSale().getDiscount());
+        int discountPrice = DiscountProduct.calculateProductSale(request.getPrice(), request.getProductSale().getDiscount());
         product.addSaleInfo(new ProductSale(request.getProductSale(), discountPrice));
 
         return new CreateProductResponse(productRepository.save(product));
---
+++ src/main/java/com/example/timedeal/utils/DiscountProduct.java
@@ -0,0 +1,8 @@
+package com.example.timedeal.utils;
+
+public class DiscountProduct{
+
+    public static int calculateProductSale(int price, int discount) {
+        return price - discount;
+    }
+}
